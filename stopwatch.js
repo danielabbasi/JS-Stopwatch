f99b57d361222601880fe86252b3b7a1aff4bed5
Daniel Abbasi <daniel_abbasi@hotmail.co.uk>
2019-09-03 15:22:23 +0100

    Added event listener for reset/lap button and added functon to reset the stopwatch to 00:00:00 after the stop button is pressed

--- stopwatch.js
+++ stopwatch.js
@@ -1,18 +1,20 @@
 var h1 = document.getElementsByTagName('h1')[0],
     time = document.getElementById('time'),
     start = document.getElementById('start'),
+    lap = document.getElementById('lap'),
     formatTime,
     interval,
     buttonState = 0,
+    buttonStateLap = 0,
     milliseconds = 0, seconds = 0, minutes = 0;
 
 
 function add() {
     milliseconds++;
-    if (milliseconds >= 100) {
+    if (milliseconds == 100) {
         milliseconds = 0;
         seconds++;
-        if (seconds >= 60) {
+        if (seconds == 60) {
             seconds = 0;
             minutes++;
         }
@@ -30,9 +32,6 @@ function pad(number) {
 
 function startClock() {
     add();
-    // console.log("milliseconds " + milliseconds)
-    // console.log("seconds " + seconds);
-    // console.log("minutes " + minutes);
     formatTime = pad(minutes) + ":" + pad(seconds) + ":" + pad(milliseconds);
     time.innerHTML = formatTime;
 }
@@ -40,12 +39,32 @@ function startClock() {
 start.addEventListener('click', function () {
     console.log("clicked")
     if (buttonState == 0) {
-        interval = setInterval(startClock, 1);
+        interval = setInterval(startClock, 10);
         this.textContent = 'Stop';
         buttonState = 1;
+        lap.textContent = 'Lap';
     } else {
         clearInterval(interval);
         this.textContent = 'Start';
         buttonState = 0;
+        lap.textContent = 'Reset';
     }
+});
+
+lap.addEventListener('click', function () {
+    if (buttonStateLap == 0 && buttonState == 0) {
+        time.innerText = "00:00:00";
+        clearInterval(interval);
+        milliseconds = 0, seconds = 0, minutes = 0;
+
+        buttonStateLap = 1;
+
+
+    } else if(buttonStateLap == 1 && buttonState == 0) {
+
+        buttonStateLap = 0
+        
+
+    }
+
 });
