70b21765c60460ae160443d12cbbab9087bdcbde
Daniel Abbasi <daniel_abbasi@hotmail.co.uk>
2019-09-03 12:36:37 +0100

    Created startClock function that uses the add function as well as formatting the time to be readable and changing the HTML to update the stopclock. Added onclick event to call startClock function every ms. Added CSS stylesheet

--- stopwatch.js
+++ stopwatch.js
@@ -1,7 +1,8 @@
 var h1 = document.getElementsByTagName('h1')[0],
     time = document.getElementById('time'),
     start = document.getElementById('start'),
-    milliseconds = 0, seconds = 0, minutes = 0;
+    formatTime,
+    milliseconds = 0, seconds = 0, minutes = 0;    
 
 
 function add(){
@@ -9,11 +10,30 @@ function add(){
     if(milliseconds >= 100){
         milliseconds = 0;
         seconds++;
-        if(seconds >= 100){
+        if(seconds >= 60){
             seconds = 0;
             minutes++;
         }
     }
 }
 
+function pad(number) {
+    if (number <= 9) {
+        return "0" + number
+    }
+    else {
+        return number
+    }
+}
 
+function startClock() {
+    add();
+    console.log("milliseconds " + milliseconds)
+    console.log("seconds " + seconds);
+    console.log("minutes " + minutes);
+    formatTime = pad(minutes) + ":" + pad(seconds) + ":" + pad(milliseconds);
+    time.innerHTML = formatTime;
+}
+startClock();
+
+document.getElementById("start").onclick = function() {setInterval(startClock, 1)};
