201b4f7d7653805f3b45afd2f558e6473f5e492c
Daniel Abbasi <daniel_abbasi@hotmail.co.uk>
2019-09-03 17:17:39 +0100

    Refactoring of code. Added some more styling as well as trying to calculate lap times correctly. Doing backup commit before timer stopwatch is changed.

--- stopwatch.js
+++ stopwatch.js
@@ -1,15 +1,15 @@
-var h1 = document.getElementsByTagName('h1')[0],
-    time = document.getElementById('time'),
-    start = document.getElementById('start'),
-    lap = document.getElementById('lap'),
-    formatTime,
-    interval,
-    buttonState = 0,
-    buttonStateLap = 0,
-    milliseconds = 0, seconds = 0, minutes = 0;
+const time = document.getElementById('time');
+const start = document.getElementById('start');
+const lap = document.getElementById('lap');
+let formatTime;
+let interval = 0;
+let buttonState = 0;
+let buttonStateLap = 0;
+let milliseconds = 0, seconds = 0, minutes = 0;
+let lapTimes = [];
 
-
-function add() {
+// Increases the timer
+function updateTime() {
     milliseconds++;
     if (milliseconds == 100) {
         milliseconds = 0;
@@ -21,36 +21,54 @@ function add() {
     }
 }
 
-function pad(number) {
-    if (number <= 9) {
-        return "0" + number
-    }
-    else {
-        return number
-    }
-}
+// Adds a preceding 0
+const pad = (number) => number < 10 ? '0' + number : number
 
+//Function that starts the stopwatch
 function startClock() {
-    add();
-    formatTime = pad(minutes) + ":" + pad(seconds) + ":" + pad(milliseconds);
-    time.innerHTML = formatTime;
+    updateTime();
+    formatTime = `${pad(minutes)}:${pad(seconds)}:${pad(milliseconds)}`
+    formatTime2 = formatTime.minutes
+    console.log(formatTime2)
+    time.textContent = formatTime;
 }
 
+//On start button click start timer function, or show stop button
 start.addEventListener('click', function () {
     console.log("clicked")
     if (buttonState == 0) {
         interval = setInterval(startClock, 10);
         this.textContent = 'Stop';
-        buttonState = 1;
+        this.style.backgroundColor = "#690000";
         lap.textContent = 'Lap';
+        buttonState = 1;
     } else {
         clearInterval(interval);
+        this.style.backgroundColor = "#006400";
         this.textContent = 'Start';
-        buttonState = 0;
         lap.textContent = 'Reset';
+        buttonState = 0;
     }
 });
 
+function calcLapTimes() {
+    if (lapTimes.length == 0) {
+        lapTimes.push(formatTime);
+        console.log("ahh")
+    } else {
+        // console.log(lapTimes[lapTimes.length - 1])
+        newLapTime = formatTime - lapTimes[lapTimes.length - 1];
+        console.log(newLapTime);
+        lapTimes.push(formatTime);
+        // lapTimes.push(formatTime - lapTimes[lapTimes.length - 1]) 
+    }
+    // lapTimes[0] = formatTime;
+    // lapTimes[1] = formatTime - lapTimes[0];
+    // lapTimes[2] = formatTime - lapTimes[1]
+    // lapTimes[3] = formatTime - lapTimes[2]
+}
+
+//On lap click, add lap or show reset
 lap.addEventListener('click', function () {
     if (buttonStateLap == 0 && buttonState == 0) {
         time.innerText = "00:00:00";
@@ -60,10 +78,17 @@ lap.addEventListener('click', function () {
         buttonStateLap = 1;
 
 
-    } else if(buttonStateLap == 1 && buttonState == 0) {
+    } else {
+        // lapTimes.push(formatTime);
+        calcLapTimes();
+        console.log(lapTimes[0]);
+        var lapList = document.querySelector('#lapList')
+        lapList.innerHTML = '<ul>' + lapTimes.map(function (lapTimes) {
+            return '<li>' + lapTimes + '<li>';
+        }).join('') + '</ul>';
 
-        buttonStateLap = 0
-        
+        buttonStateLap = 0;
+
 
     }
 
