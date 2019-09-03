15c3bdf339be4c6752d2cfc3a7903f74b0310f59
Daniel Abbasi <daniel_abbasi@hotmail.co.uk>
2019-09-03 12:04:03 +0100

    Created add function to JS to add milliseconds, seconds and minutes and set the variables accordingly

--- stopwatch.js
+++ stopwatch.js
@@ -1,7 +1,19 @@
-var h1 = document.getElementsByTagName('h1'),
+var h1 = document.getElementsByTagName('h1')[0],
+    time = document.getElementById('time'),
     start = document.getElementById('start'),
     milliseconds = 0, seconds = 0, minutes = 0;
 
 
+function add(){
+    milliseconds++;
+    if(milliseconds >= 100){
+        milliseconds = 0;
+        seconds++;
+        if(seconds >= 100){
+            seconds = 0;
+            minutes++;
+        }
+    }
+}
 
 
