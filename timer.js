f2bbfe932d65e54f45e481212c57e0474f276184
Daniel Abbasi <daniel_abbasi@hotmail.co.uk>
2019-09-04 16:56:45 +0100

    Figured out calculating lap times by using two arrays and using the reduce method to find the difference between current times which are recorded every time the user clicks lap

--- timer.js
+++ timer.js
@@ -4,58 +4,74 @@ class Timer {
         this.timeElapsed = 0;
         this.lapTimePress = 0;
         this.lapTimesArray = [];
+        this.lapTimesDifferenceArray = [];
+        this.timeText = document.getElementById('time');
+        this.startStopText = document.getElementById('startStop');
+        this.lapResetText = document.getElementById('lapReset');
+        this.timeText = document.getElementById('time');
 
         this.updateTime = () => {
             this.timeElapsed++;
+            this.timeText.textContent = this.millisecondConversion(this.timeElapsed);
+        }
+    }
+    
+    
+    millisecondConversion(timeElapsed) {
+        var milliseconds = timeElapsed % 100;
+        var seconds = Math.floor((timeElapsed / 100) % 60);
+        var minutes = Math.floor((timeElapsed / (60 * 100)) % 60);
+        const pad = (time) => time < 10 ? '0' + time : time
+        
+        return pad(minutes) + ":" + pad(seconds) + "." + pad(milliseconds);
+    }
+
+    startStop() {
+        if (this.isRunning == false) {
+            this.interval = setInterval(this.updateTime, 10);
+            this.isRunning = true;
+            this.startStopText.textContent = 'Stop';
+            this.lapResetText.textContent = 'Lap';
+
+        }
+        else {
+            clearInterval(this.interval)
+            this.isRunning = false;
+            this.startStopText.textContent = 'Start';
+            this.lapResetText.textContent = 'Reset';
+        }
+    }    
+
+    lapReset() {
+        if (this.isRunning == false) {
+            //Reset hit
+            this.timeText.textContent = '00:00:00';
+            clearInterval(this.interval);
+            this.timeElapsed = 0;
+        }
+        else {
+            //Lap button clicked
+            this.lapTimesArray.push(this.timeElapsed);
+            let difference = [];
+            this.lapTimesArray.reduce((a, b) => {
+                difference.push(b - a)
+                return b
+            })
+            console.log("difference " + difference[difference.length-1]);
         }
     }
 
-    startPressed() {
-        this.isRunning = true;
-        this.interval = setInterval(this.updateTime, 10);
-    }
+    // calcLapTimes() {
+    //     if (this.lapTimesArray.length == 0) {
+    //         console.log("New array created")
+    //         this.lapTimesArray.push(this.timeElapsed);
+    //     } else {
+    //     }
+    // }
 
-    stopPressed() {
-        this.isRunning = false;
-        clearInterval(this.interval)
-    }
+    DOMElement() {
 
-    calcLapTimes() {
-        if (this.lapTimesArray.length == 0) {
-            this.lapTimesArray.push(this.timeElapsed)
-        } else {
-            this.lapTimesArray.push(this.timeElapsed - this.lapTimesArray[-1])
-        }
-    }
-
-    lapPressed() {
-        
-    }
-
-    resetPressed() {
-        
     }
 }
 
 const timer = new Timer();
-
-startStop = () => {
-    if (timer.isRunning == false) {
-        timer.startPressed();
-    }
-    else {
-        timer.stopPressed();
-
-    }
-
-lapReset = () => {
-    if (timer.isRunning == false) {
-        
-    }
-    else {
-        lapPressed();
-    }
-}
-    console.log(timer.isRunning)
-    console.log(timer.timeElapsed)
-}
