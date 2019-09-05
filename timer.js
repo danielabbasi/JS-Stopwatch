2e67651b21d242b1b45d708233dfa711382dcc93
Daniel Abbasi <daniel_abbasi@hotmail.co.uk>
2019-09-05 10:23:27 +0100

    Refactored code to seperate button changes and button text changes into functions outside the timer class. Created onUpdate() function that uses these other functions making the code much more readable and easier to test. passing onUpdate function into the Timer constructer class therefore allowing us to call it in the updateTime and startStop functions

--- timer.js
+++ timer.js
@@ -1,72 +1,87 @@
 class Timer {
-    constructor(){
+    constructor(onUpdate){
         this.isRunning = false;
         this.timeElapsed = 0;
         this.lapTimePress = 0;
+        this.onUpdate = onUpdate; // IMPORTANT
         this.lapTimesArray = [];
         this.lapTimesDifferenceArray = [];
 
         this.updateTime = () => {
-            this.timeElapsed++;
-            document.getElementById('time').textContent = this.millisecondConversion(this.timeElapsed);
+            this.isRunning = true
+
+            this.interval = setInterval(() => {
+                this.timeElapsed++
+                this.onUpdate()
+                console.log(onUpdate);
+            }, 10)
+        }
+
+        this.stopTime = () => {
+            clearInterval(this.interval)
+            this.isRunning = false
+            this.onUpdate()
         }
     }
     
-    millisecondConversion(timeElapsed) {
-        var milliseconds = timeElapsed % 100;
-        var seconds = Math.floor((timeElapsed / 100) % 60);
-        var minutes = Math.floor((timeElapsed / (60 * 100)) % 60);
-        const pad = (time) => time < 10 ? '0' + time : time
-        
-        return pad(minutes) + ":" + pad(seconds) + "." + pad(milliseconds);
-    }
 
     startStop() {
-        if (this.isRunning == false) {
-            this.interval = setInterval(this.updateTime, 10);
-            this.isRunning = true;
-            document.getElementById('startStop').textContent = 'Stop';
-            document.getElementById('lapReset').textContent = 'Lap';
+        if (this.isRunning === false) {
+            this.updateTime()
         }
         else {
-            clearInterval(this.interval)
-            this.isRunning = false;
-            document.getElementById('startStop').textContent = 'Start';
-            document.getElementById('lapReset').textContent = 'Reset';
+            this.stopTime()
         }
     }    
 
-    lapReset() {
-        if (this.isRunning == false) {
-            //Reset hit
-            document.getElementById('time').textContent = '00:00.00';
-            clearInterval(this.interval);
-            this.timeElapsed = 0;
-        }
-        else {
-            //Lap button clicked
-            this.lapTimesArray.push(this.timeElapsed);
-            // this.lapTimesArray.reduce((a, b) => {
-            //     this.lapTimesDifferenceArray.push(b - a)
-            //     return b
-            // })
+    // lapReset() {
+    //     if (this.isRunning == false) {
+    //         clearInterval(this.interval);
+    //         this.timeElapsed = 0;
+    //     }
+    //     else {
+    //         //Lap button clicked
+    //         this.lapTimesArray.push(this.timeElapsed);
 
-            this.lapTimesDifferenceArray = this.lapTimesArray.map(lap = => {
-                return lap - 
-            } )
+    //         this.lapTimesDifferenceArray = this.lapTimesArray.map(lap = => {
+    //             return lap - 
+    //         } )
 
-            
-
-            console.log(this.lapTimesDifferenceArray);
-            let currentLap = this.lapTimesDifferenceArray.pop
-            console.log("current lap " + currentLap);
+    //         console.log(this.lapTimesDifferenceArray);
+    //         let currentLap = this.lapTimesDifferenceArray.pop
+    //         console.log("current lap " + currentLap);
         
-            lapList.innerHTML = '<ul>' + currentLap.map(function (lapTimess) {
-            return '<li>' + lapTimess + '<li>';
-            }).join('') + '</ul>';
-            console.log("difference " + this.lapTimesDifferenceArray[this.lapTimesDifferenceArray.length - 1]);
-        }
-    }
+    //         lapList.innerHTML = '<ul>' + currentLap.map(function (lapTimess) {
+    //         return '<li>' + lapTimess + '<li>';
+    //         }).join('') + '</ul>';
+    //         console.log("difference " + this.lapTimesDifferenceArray[this.lapTimesDifferenceArray.length - 1]);
+    //     }
+    // }
 }
 
-const timer = new Timer();
+timer = new Timer(onUpdate);
+
+function onUpdate() {
+    updateTimeDisplay(millisecondConversion(timer.timeElapsed))
+    updateStartStopButton(updateStartStopButtonText(timer.isRunning))
+    updateLapResetButton(updateLapResetButtonText(timer.timeElapsed, timer.isRunning))    
+}
+
+const millisecondConversion = (timeElapsed) => {
+    var milliseconds = timeElapsed % 100;
+    var seconds = Math.floor((timeElapsed / 100) % 60);
+    var minutes = Math.floor((timeElapsed / (60 * 100)) % 60);
+    const pad = (time) => time < 10 ? '0' + time : time
+    
+    return pad(minutes) + ":" + pad(seconds) + "." + pad(milliseconds);
+}
+
+const updateStartStopButtonText = (isRunning) => isRunning ? 'Stop' : 'Start';
+
+const updateStartStopButton = (text) => document.getElementById('startStop').textContent = text;
+
+const updateLapResetButtonText = (timeElapsed, isRunning) => timeElapsed && isRunning > 0 ? 'Lap' : 'Reset';
+
+const updateLapResetButton = (text) => document.getElementById('lapReset').textContent = text;
+
+const updateTimeDisplay = (time) => document.getElementById('time').textContent = time;
